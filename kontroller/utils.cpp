becbadd5df2ff0b715326b30511cf5cde70d6794
Julien Blanc <julien.blanc@tgcm.eu>
2020-05-10 09:40:33 +0200

    Display images when using password auth - refs #6
    * thumbnails should now be displayed correctly

--- kontroller/utils.cpp
+++ kontroller/utils.cpp
@@ -1,5 +1,6 @@
 #include "utils.h"
 
+#include "server.h"
 
 namespace eu
 {
@@ -12,8 +13,16 @@ QUrl getImageUrl(Client* client, QString kodiUrl)
 {
 	if(kodiUrl.size() > 0)
 	{
+		if (client->server()->password().size() > 0)
+		{
+			QString url = QString("http://") + QUrl::toPercentEncoding(client->server()->login()) + ":" +
+			              QUrl::toPercentEncoding(client->server()->password()) + "@" +
+			              client->serverAddress() + ":" + QString::number(client->serverHttpPort()) + "/image/" +
+			              QUrl::toPercentEncoding(kodiUrl);
+			return QUrl(url);
+		}
 		QString url = QString("http://") + client->serverAddress() + ":" + QString::number(client->serverHttpPort()) +
-		              "/image/" +QUrl::toPercentEncoding(kodiUrl);
+		              "/image/" + QUrl::toPercentEncoding(kodiUrl);
 		return QUrl{url};
 	}
 	else
